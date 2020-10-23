179e61be3aa594f8125dd9eef29be24c93173c9a
Vincent <vrdphone@gmail.com>
2020-10-22 20:51:37 -0500

    added users validation

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const router = express.Router();
+// const { check, validationResult } = require('express-validator/check');
+const { check, validationResult } = require('express-validator');
 
-//@route GET api/users    @desc  test route     @access Public
-router.get('/', (request, response) => {
+//@route POST api/users    @desc  register user     @access Public
+router.post('/', [
+    check('name', 'Name is required').not().isEmpty(),
+    check('email', 'Please include a valid email').isEmail(),
+    check('password', 'Please enter a password with 6 or more characters').isLength({ min: 6 })
+], (request, response) => {
+    const errors = validationResult(request);
+    if (!errors.isEmpty()) {
+        return response.status(400).json({ errors: errors.array() });
+    }
+    console.log(request.body);
     response.send('user route');
 });
 
